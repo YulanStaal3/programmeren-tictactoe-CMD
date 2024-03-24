807f3cd08ff7cc8bcfe682db353fd3f7219f392c
YulanStaal3 <112861488+YulanStaal3@users.noreply.github.com>
2024-03-24 21:21:16 +0100

    helppppp

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
+console.log("testest");
+
 // speler x begint altijd
 let currentPlayer = 'X'; 
-
 
 // 3x3 bordspel
 let gameBoard = ['', '', '', '', '', '', '', '', '']; 
 
-
-// spel is nog actief
+// spel is actief
 let gameActive = true;
 
-
-// speler gaat vakje klikken
+// speler gaat op vakje klikken
 function handlePlayerTurn(clickedCellIndex) {
   // checkt of cel leeg is en of spel actief is
   if (gameBoard[clickedCellIndex] !== '' || !gameActive) {
@@ -24,34 +23,49 @@ function handlePlayerTurn(clickedCellIndex) {
   currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
 }
 
-
+// speler klikt op vakje
 function cellClicked(clickedCellEvent) {
+  // checkt of vakje leeg is en spel actief is
   const clickedCell = clickedCellEvent.target;
   const clickedCellIndex = parseInt(clickedCell.id.replace('cell-', '')) - 1;
   if (gameBoard[clickedCellIndex] !== '' || !gameActive) {
       return;
   }
+  // functie wordt aangeroepen die beurt van speler behandelt, ui wordt upgedate
   handlePlayerTurn(clickedCellIndex);
   updateUI();
 }
 
+// ophalen html
 const cells = document.querySelectorAll('.cell');
 
 cells.forEach(cell => {
   cell.addEventListener('click', cellClicked, false);
 });
 
+
+// Ui wordt opgedate, x of o in vakje
 function updateUI() {
   for (let i = 0; i < cells.length; i++) {
+    // dom manipulatie
       cells[i].innerText = gameBoard[i];
   }
 }
 
+// speler wint
 function announceWinner(player) {
   const messageElement = document.getElementById('gameMessage');
-  messageElement.innerText = `Player ${player} Wins!`;
+  messageElement.innerText = `Speler ${player} wint!`;
+
+  // Als speler X wint, verandert de achtergrondkleur van mn h1-element naar rood
+  if (player === 'X') {
+    messageElement.style.backgroundColor = 'red';
+  } else { // Als speler O wint, verander de achtergrondkleur van het h1-element naar blauw
+    messageElement.style.backgroundColor = 'blue';
+  }
 }
 
+// gelijk spel
 function announceDraw() {
   const messageElement = document.getElementById('gameMessage');
   messageElement.innerText = 'Game Draw!';
@@ -59,16 +73,17 @@ function announceDraw() {
 
 // combinaties om te winnen
 const winConditions = [
-  [0, 1, 2], // Top row
-  [3, 4, 5], // Middle row
-  [6, 7, 8], // Bottom row
-  [0, 3, 6], // Left column
-  [1, 4, 7], // Middle column
-  [2, 5, 8], // Right column
-  [0, 4, 8], // Left-to-right diagonal
-  [2, 4, 6]  // Right-to-left diagonal
+  [0, 1, 2], 
+  [3, 4, 5], 
+  [6, 7, 8], 
+  [0, 3, 6], 
+  [1, 4, 7], 
+  [2, 5, 8], 
+  [0, 4, 8], 
+  [2, 4, 6]  
 ];
 
+// functie controleert of je gewonnen of gelijk spel speelt
 function checkForWinOrDraw() {
   let roundWon = false;
 
@@ -94,6 +109,7 @@ function checkForWinOrDraw() {
   }
 }
 
+// functie die de game reset en dus alles leeg maakt etc
 function resetGame() {
   gameBoard = ['', '', '', '', '', '', '', '', ''];
   gameActive = true;
@@ -106,3 +122,9 @@ function resetGame() {
 
 const resetButton = document.getElementById('resetButton');
 resetButton.addEventListener('click', resetGame, false);
+
+// bronnen
+// - https://www.youtube.com/watch?v=knkWr93kClY
+// - https://www.youtube.com/watch?v=oZrp3Atkz18
+// - https://www.geeksforgeeks.org/simple-tic-tac-toe-game-using-javascript/
+
